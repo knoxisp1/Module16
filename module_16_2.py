6146317a9710c23adefd24807c7d2e92ef72d2e7
Eduard <knoxwillesp@gmail.com>
2025-02-04 18:24:51 +0300

    Update module_16_2.py

--- module_16_2.py
+++ module_16_2.py
@@ -1,25 +1,27 @@
-from fastapi import Path, FastAPI
+from fastapi import FastAPI, Path
 from typing import Annotated
 
 app = FastAPI()
 
 
 @app.get("/")
-async def get_main_page() -> dict:
-    return {"message": "Главная страница"}
+async def main_page():
+    return "Главная страница"
 
 
 @app.get("/user/admin")
-async def get_admin_page() -> dict:
-    return {"message": "Вы вошли как администратор"}
+async def admin_page():
+    return "Вы вошли как администратор"
 
 
 @app.get("/user/{user_id}")
-async def get_id_user(user_id: Annotated[int, Path(ge=1, le=100, description="Enter User ID")]) -> dict:
-    return {"message": f"Вы вошли  как пользователь № {user_id}"}
+async def get_user_id(
+        user_id: Annotated[int, Path(ge=1, le=100, description="Enter User ID")]):
+    return f"Вы вошли как пользователь № {user_id} "
 
 
 @app.get("/user")
-async def get_user_info(username: Annotated[str, Path(min_length=5, max_length=20, description="Enter Username")],
-                        age: Annotated[int, Path(ge=18, le=120, description="Enter age")]) ->dict:
-    return {"message": f"Информация о пользователе.Имя:{username},Возраст:{age}"}
+async def user_info(
+        username: Annotated[str, Path(max_length=20, min_length=5, description="Enter username")],
+        age: Annotated[int, Path(ge=18, le=120, description="Enter age")]):
+    return f"Информация о пользователе. Имя:{username},Возраст:{age}"
