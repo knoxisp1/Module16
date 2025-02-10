04c80cb43580037d09d988ce310c5ab0312d4189
Eduard <knoxwillesp@gmail.com>
2025-02-10 17:49:02 +0300

    Update module_16_1.py

--- module_16_1.py
+++ module_16_1.py
@@ -1,24 +1,21 @@
-# Импорт фастапи
 from fastapi import FastAPI
 
-app = FastAPI()
+app=FastAPI()
 
 
 @app.get("/")
 async def main_page():
     return "Главная страница"
 
-
 @app.get("/user/admin")
 async def admin_page():
-    return "Вы вошли как администратор"
-
+    return "Вы вошли как Администратор"
 
 @app.get("/user/{user_id}")
-async def get_user_id(user_id: int):
-    return f"Вы вошли как пользователь №{user_id}"
-
+async def get_user_id(user_id:int):
+    return f"Вы вошли как пользователь № {user_id}"
 
 @app.get("/user")
-async def user_info(username: str, age: int):
-    return f"Информация о пользователе.Имя:{username},Возраст:{age}"
+async def get_user_info(username:str ,age:int):
+    return f"Информация о пользователе. Имя:{username},Возраст:{age}"
+
